1d498595a8299fabb1750eab1ac6074deb24be92
Dimitri Sokolyuk <demon@dim13.org>
2016-11-12 12:48:19 +0100

    Consolidate templates

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 	"time"
 )
 
-// ISC style license
-const ISC = `Copyright (c) {{.Year}} {{.Name}}{{with .Mail}} <{{.}}>{{end}}
+// License is ISC style license
+const License = `{{define "license" -}}
+Copyright (c) {{.Year}} {{.Name}}{{with .Mail}} <{{.}}>{{end}}
 
 Permission to use, copy, modify, and distribute this software for any
 purpose with or without fee is hereby granted, provided that the above
@@ -24,13 +25,19 @@ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
 WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-`
-
-// Short banner
-const Short = `// Copyright (c) {{.Year}} {{.Name}}. All rights reserved.
+{{end}}
+{{define "banner" -}}
+// Copyright (c) {{.Year}} {{.Name}}. All rights reserved.
 // Use of this source code is governed by ISC-style license
 // that can be found in the LICENSE file.
+{{end}}
 `
+
+type params struct {
+	Name string
+	Mail string
+	Year int
+}
 
 func main() {
 	usr, err := user.Current()
@@ -46,21 +53,17 @@ func main() {
 	name := flag.String("name", usr.Name, "Full name")
 	mail := flag.String("mail", usr.Username+"@"+host, "Mail address")
 	year := flag.Int("year", time.Now().Year(), "Copyright year")
-	short := flag.Bool("short", false, "Short version")
+	banner := flag.Bool("banner", false, "Print banner")
 	flag.Parse()
 
-	var tmpl *template.Template
-	if *short {
-		tmpl = template.Must(template.New("Short").Parse(Short))
-	} else {
-		tmpl = template.Must(template.New("ISC").Parse(ISC))
+	tmpl := template.Must(template.New("lic").Parse(License))
+	kind := "license"
+	if *banner {
+		kind = "banner"
 	}
 
-	err = tmpl.Execute(os.Stdout, struct {
-		Name, Mail string
-		Year       int
-	}{*name, *mail, *year})
-	if err != nil {
+	p := params{*name, *mail, *year}
+	if err = tmpl.ExecuteTemplate(os.Stdout, kind, p); err != nil {
 		log.Fatal(err)
 	}
 }
